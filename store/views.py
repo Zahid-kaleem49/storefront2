3d57d2facaf8f4af8f89c2c968b0a129ee6ad498
Zahid-kaleem49 <zahid.kaleem49@gmail.com>
2024-03-15 08:46:35 +0500

    initial commit

---
+++ store/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
+
+
+# Create your views here.
+
+
+@api_view(['Get'])
+def product_list(request):
+    query_set = Product.objects.all()
+    serializer = ProductSerializer(query_set, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['Get'])
+def product_details(request, id):
+    # try:
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+    # except Exception as e:
+    # return Response(status=status.HTTP_404_NOT_FOUND )
+
